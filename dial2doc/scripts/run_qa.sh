a81226bfecbbacce449c595a3c46f05c46fd5981
ameyasm1154 <ameyasm1154@gmail.com>
2021-11-09 19:55:32 +0000

    dial2doc train and eval scripts

---
+++ dial2doc/scripts/run_qa.sh
@@ -0,0 +1,23 @@
+python run_qa.py \
+ --dataset_name '../datasets/doc2dial/doc2dial.py' \
+ --dataset_config_name doc2dial_rc \
+ --model_name_or_path bert-base-uncased \
+ --do_train \
+ --do_eval \
+ --version_2_with_negative \
+ --logging_steps 2000 \
+ --save_steps 2000 \
+ --learning_rate 3e-5  \
+ --num_train_epochs 2 \
+ --max_seq_length 512  \
+ --max_answer_length 50 \
+ --doc_stride 128  \
+ --cache_dir bert \
+ --output_dir bert \
+ --overwrite_output_dir  \
+ --per_device_train_batch_size 8 \
+ --per_device_train_batch_size 8 \
+ --gradient_accumulation_steps 2  \
+ --warmup_steps 1000 \
+ --weight_decay 0.01  \
+ --fp16 
